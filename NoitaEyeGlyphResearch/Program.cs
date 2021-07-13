ab1efbae19f8f4cb485b6a4327ebe1a8e2da1109
Doctor Ned <1933864+Doctor-Ned@users.noreply.github.com>
2021-07-13 21:58:29 +0200

    Add base8 test strings

--- NoitaEyeGlyphResearch/Program.cs
+++ NoitaEyeGlyphResearch/Program.cs
@@ -19,15 +19,32 @@ namespace NoitaEyeGlyphResearch {
                 Encoding encoding = Encoding.ASCII;
                 const int KEY_COUNT = 35;
                 const string alph25 = "abcdefghiklmnopqrstuvwxyz";
+                string[] msgs = {
+                    "SWBQWFÖNIVOWWWMG!ED?CNFRB.?MX!FHAMTVKGFWPQQOHQFPHRYLBIDPXXNRNUFKKBJEXDÄUEÄHXETNP.YWX“!DEQLKENVMFYPQ",
+                    ".WBQWFÖNIVOWWWMG!ED?CNFRBNDKU!FHAJKWEGFWPQQOHQFPHRQKX!YP“MYYBKZHXCNJY!YOFMFFFYFFOXVOW?DCZWCDVYNQCPYWFOG",
+                    "MWBQWFÖNIVOWWWMG!ED?CNFRBZ.UFQW.GYVMR?DEUHPM.CFYNRYXCRLFECNIK!CU!VOWWFDEW!ONKGADNYVEHVVNHGYOBDK?EBWN!JYVEQWTEXJDKNVUOO",
+                    ".WBRÖWZOMBOUKQS.MITVPEFERWOJMDUTEFEWF?FQNUV?O.BPXD.!P?OJWGCRWYU!DWWFPFUWKHMLYHLMSCBJENPPK.VY.FDNXGVXUE",
+                    "XWBRÖWCUNM?PUÖXXUCVDPGNÄXN.FUF!GLSMPCXQL.C.ÖBUXDFEMVNÖTPUOFDGVCCWFWBWUXYJ“DEHYCWZKXEEKVNEFGXGOYXN!?G?MPFECFBM“XBYZDPEWULGYWGQ?ZBAOEXZJLEI",
+                    "OWBRÖWHÄDFMKHQXVNUOMÄCFMDGHPH!OEWFORMT.WEPPFX“FMQEKNKMNYVOTCZWECOPGPPPDFNKLWNANBOTNFMJFFDYB“OYDXDQDXVMGGLSVPDCWRTVFZMÖÄWIKWW",
+                    "LWBRÖWCUNMCTFDXGYDMBP!QOBGM.EOUXW.FW“ÖQWTHWYGPTWÖBDPNWYZ“JONZPYSON?K“VEUGUGÖPUXXXORNMV“!DDKWL?KVZMEHKFXOOYDPVUJC.WY!X?H",
+                    "!WBRÖWCUNMCTFDXGYDMBPUF.BYTE?LVHBWGLPFÄ!PG?AJVPWYO!HISVKXÖKYWVUSXZGFEMKXBNHPWPVOWHZLWO!XDX“CGFTZÄFBFXNNFUG?QKEEOLBLWOEMK",
+                    "NWBRÖWCUNMCTFDXGYDMBPNFXQGM.XOUX“LEWBRQWTUWYZCVGWQXVDOMKLHKEOODKXTOÖWFCUBHJNO.JEG.ETWFMFNCQZENWZCLFYBMVTOWYÄVJXOIN"
+
+                };
+
+                foreach (string msg in msgs) {
+                    string alphabet = new string(msg.Distinct().OrderBy(c => c).ToArray());
+                    Console.WriteLine(msg.GetIc(alphabet));
+                }
 
                 foreach (ReorderParam param in Enum.GetValues(typeof(ReorderParam))) {
                     foreach (TrigramCollection line in tlc.Reorder(param, param).TrigramLines.Take(1)) {
-                        for (byte i = 0; i < 3; ++i) {
-                            string msg = new string(line.Trigrams.Select(t => t.GetBase10()).Where(n => (n / 25) == i)
-                                .Select(n => alph25[n % 25]).ToArray());
-                            Console.WriteLine(msg);
-                            Console.WriteLine(msg.GetIc(alph25));
-                        }
+                        //for (byte i = 0; i < 3; ++i) {
+                        //    string msg = new string(line.Trigrams.Select(t => t.GetBase10()).Where(n => (n / 25) == i)
+                        //        .Select(n => alph25[n % 25]).ToArray());
+                        //    Console.WriteLine(msg);
+                        //    Console.WriteLine(msg.GetIc(alph25));
+                        //}
                     }
                 }
 
